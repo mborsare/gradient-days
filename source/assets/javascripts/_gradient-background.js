1103cacedb65d4f2391c43dd9a472fcfeaf6b183
Mike Borsare <mborsare@gmail.com>
2016-12-16 11:49:33 -0500

    MVP teaser page

---
+++ source/assets/javascripts/_gradient-background.js
@@ -0,0 +1,7 @@
+$(document).mousemove(function(e){
+  var $width = ($(document).width())/255;
+  var $height = ($(document).height())/255;
+  var $pageX = parseInt(e.pageX / $width,10);
+  var $pageY = parseInt(e.pageY / $height,10);
+  $("body").css("background", "linear-gradient(135deg, rgb(112, "+$pageX+", "+$pageY+"), rgb(255, "+$pageY+", "+$pageX+")" );
+});
